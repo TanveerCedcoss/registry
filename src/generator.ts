c04fcb72c438594da7d6423e6e6890433f7628b7
Tyler van Hensbergen <tyler@stedi.com>
2022-02-01 16:22:12 -0700

    poc: add ramp schema and provider

--- src/generator.ts
+++ src/generator.ts
@@ -1,27 +1,17 @@
 import { SchemaPackage } from "./provider";
 import providers from "./providers";
 import openAPIParser from "@readme/openapi-parser";
-import refParser, { JSONSchema } from "@apidevtools/json-schema-ref-parser";
-import { OpenAPIV3 } from "openapi-types";
 import path from "path";
 import fs from "fs";
 
-type Schema = {
+interface Schema {
   name: string;
   schema: unknown;
-};
+}
 
 async function listVersions(providerName: keyof typeof providers) {
   return await providers[providerName].getVersions();
 }
-
-// function extractSchemas(definition: OpenAPIV3.Document): JSONSchema {
-//   const dereferenced = openAPIParser.dereference(definition, {
-//     dereference: { circular: "ignore" },
-//   });
-//   const schemas: OpenAPIV3.ComponentsObject["schemas"] =
-//     definition.components?.schemas ?? {};
-// }
 
 async function unbundle(bundle: SchemaPackage): Promise<Schema[]> {
   switch (bundle.type) {
@@ -46,15 +36,19 @@ export async function generateForVersion(
   providerName: keyof typeof providers,
   version: string
 ) {
+  const baseDir = path.join(rootPath, providerName, version);
+  const folderExists = !fs.mkdirSync(baseDir, { recursive: true });
+  if (folderExists) {
+    console.log(`Skipping [${providerName}, ${version}]...`);
+    return;
+  }
   const bundle = await providers[providerName].getSchema(version);
   const schemas = await unbundle(bundle);
-  const baseDir = path.join(rootPath, providerName, bundle.versionName);
-  fs.mkdirSync(baseDir, { recursive: true });
+
+  console.log(`Generating [${providerName}, ${version}]...`);
   for (const schema of schemas) {
-    fs.writeFileSync(
-      path.join(baseDir, `${schema.name}.json`),
-      JSON.stringify(schema.schema, null, 2)
-    );
+    const target = path.join(baseDir, `${schema.name}.json`);
+    fs.writeFileSync(target, JSON.stringify(schema.schema, null, 2));
   }
 }
 
@@ -64,11 +58,11 @@ export async function generateAll(
 ) {
   const versions = await listVersions(providerName);
   for (const version of versions) {
-    console.log(`Generating [${providerName}, ${version}]...`);
     await generateForVersion(rootPath, providerName, version);
   }
 }
 
 (async () => {
   await generateAll("./schemas", "stripe");
+  await generateAll("./schemas", "ramp");
 })();
--- src/provider.ts
+++ src/provider.ts
@@ -1,3 +1,6 @@
+import http from "axios";
+import { OpenAPIV3 } from "openapi-types";
+
 export interface OpenAPI3Schema {
   type: "openapi-v3";
   versionName: string;
@@ -9,4 +12,27 @@ export type SchemaPackage = OpenAPI3Schema;
 export interface Provider {
   getVersions: () => Promise<string[]>;
   getSchema: (version: string) => Promise<SchemaPackage>;
+}
+
+export function openAPIUrlProvider(
+  versions: Record<
+    string,
+    {
+      url: string;
+      transform?: (definition: unknown) => Promise<OpenAPIV3.Document>;
+    }
+  >
+): Provider {
+  return {
+    getVersions: async () => Object.entries(versions).map(([k, _]) => k),
+    getSchema: async (version) => {
+      const { url, transform } = versions[version];
+      const { data } = await http.get(url);
+      return {
+        type: "openapi-v3",
+        versionName: version,
+        value: !!transform ? await transform(data) : data,
+      };
+    },
+  };
 }
